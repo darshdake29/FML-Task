50612103e675be9db0ae23b56cf1d6234af9482c
TusharPatell <pateltushar9727@gmail.com>
2022-03-06 19:48:55 +0530

    Task-3 is being completed

---
+++ index.py
@@ -0,0 +1,11 @@
+# this is task-3
+a, b = 2, 3
+def swapNum(a, b):
+    """This function is for swapping the numbers"""
+    a, b = b, a  # Swapping logic
+    return a, b
+
+
+ans = swapNum(a, b)
+print(ans)
+
